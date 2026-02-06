98238dc3d75d2c3e0a14df4a97c9bac5eb767e71
Maestro <maestro@example.com>
2026-02-06 22:57:28 +0900

    Add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // あなたのGitHub URLのドメイン部分
+  site: 'https://digital-old-masters.github.io',
+  // あなたのリポジトリ名（サブディレクトリ）
+  base: '/digital-old-masters',
+});
